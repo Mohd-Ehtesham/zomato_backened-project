2dca1456e3f827af97983f0d1ea0882de0203c17
Mohd Ehtesham <ehteshamm56@gmail.com>
2024-09-20 10:52:13 +0530

    increasing pool size time

--- server/src/connector.js
+++ server/src/connector.js
@@ -9,7 +9,7 @@ const createPool = (database) => {
     user: process.env.USER, // MariaDB username
     password: process.env.PASSWORD, // MariaDB password
     database: database || undefined, // Optional: specify a database if provided
-    connectionLimit: 5, // Limit the number of connections in the pool
+    connectionLimit: 10, // Limit the number of connections in the pool
   });
 };
 console.log(createPool);
